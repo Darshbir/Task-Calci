c3d5358cfaec8ab6456277c4cf98ffd5c8c6249f
Darshbir Singh <darshbir86@gmail.com>
2024-08-09 15:38:37 +0530

    Working Mechanism Added

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
-# first_app
+# Calculator app
 
-A new Flutter project.
+Just made the UI/UX for a calculator app in flutter.
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Edit: Converted it into a working model and attached with firebase to store history of calculations
--- lib/Calculator_Screen.dart
+++ lib/Calculator_Screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:first_app/btn_classes.dart';
 import 'package:flutter/material.dart';
 
@@ -9,82 +10,276 @@ class CalculatorScreen extends StatefulWidget {
 }
 
 class _CalculatorScreenState extends State<CalculatorScreen> {
+  String number1 = "";
+  String operand = "";
+  String number2 = "";
+  List<String> history = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadHistory();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final Screensize = MediaQuery.of(context).size;
+    final screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      body: SafeArea(
-        bottom: false,
-        child: Column(children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Container(
-                alignment: Alignment.bottomRight,
-                padding: const EdgeInsets.all(16),
-                child: const Text(
-                  "0",
-                  style: TextStyle(
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.end,
-                ),
+      appBar: AppBar(
+        title: const Text("Calculator"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              child: Text(
+                'History',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blue,
               ),
             ),
-          ),
-          Wrap(
-            children: Btn.buttonValues
+            ...history
                 .map(
-                  (value) => SizedBox(
-                    width: value == Btn.n0
-                        ? (Screensize.width / 2)
-                        : (Screensize.width / 4),
-                    height: Screensize.width / 4.5,
-                    child: buildButton(value),
+                  (item) => ListTile(
+                    title: Text(item),
                   ),
                 )
                 .toList(),
-          ),
-        ]),
+          ],
+        ),
       ),
-    );
-  }
-
-  Widget buildButton(value) {
-    return Padding(
-      padding: const EdgeInsets.all(4.0),
-      child: Material(
-        color: getBtnColour(value),
-        clipBehavior: Clip.hardEdge,
-        shape: OutlineInputBorder(
-            borderSide: const BorderSide(
-              color: Colors.white24,
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                reverse: true,
+                child: Container(
+                  alignment: Alignment.bottomRight,
+                  padding: const EdgeInsets.all(16),
+                  child: Text(
+                    "$number1$operand$number2".isEmpty
+                        ? "0"
+                        : "$number1$operand$number2",
+                    style: const TextStyle(
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.end,
+                  ),
+                ),
+              ),
             ),
-            borderRadius: BorderRadius.circular(100)),
-        child: InkWell(
-          onTap: () {},
-          child: Center(
-              child: Text(
-            value,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
-          )),
+            Wrap(
+              children: [
+                ...Btn.buttonValues.map(
+                  (value) => SizedBox(
+                    width: value == Btn.n0
+                        ? screenSize.width / 2
+                        : (screenSize.width / 4),
+                    height: screenSize.width / 5,
+                    child: buildButton(value),
+                  ),
+                )
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
-}
 
-Color getBtnColour(value) {
-  return [Btn.del, Btn.clr].contains(value)
-      ? Colors.blueGrey
-      : [
-          Btn.per,
-          Btn.multiply,
-          Btn.add,
-          Btn.subtract,
-          Btn.divide,
-          Btn.calculate,
-        ].contains(value)
-          ? Colors.orange
-          : Colors.black87;
+  Widget buildButton(String value) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Material(
+        color: getBtnColor(value),
+        clipBehavior: Clip.hardEdge,
+        shape: OutlineInputBorder(
+          borderSide: const BorderSide(
+            color: Colors.white24,
+          ),
+          borderRadius: BorderRadius.circular(100),
+        ),
+        child: InkWell(
+          onTap: () => onBtnTap(value),
+          child: Center(
+            child: Text(
+              value,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onBtnTap(String value) {
+    if (value == Btn.del) {
+      delete();
+      return;
+    }
+
+    if (value == Btn.clr) {
+      clearAll();
+      return;
+    }
+
+    if (value == Btn.per) {
+      convertToPercentage();
+      return;
+    }
+
+    if (value == Btn.calculate) {
+      calculate();
+      return;
+    }
+
+    appendValue(value);
+  }
+
+  void calculate() {
+    if (number1.isEmpty) return;
+    if (operand.isEmpty) return;
+    if (number2.isEmpty) return;
+
+    final double num1 = double.parse(number1);
+    final double num2 = double.parse(number2);
+
+    var result = 0.0;
+    switch (operand) {
+      case Btn.add:
+        result = num1 + num2;
+        break;
+      case Btn.subtract:
+        result = num1 - num2;
+        break;
+      case Btn.multiply:
+        result = num1 * num2;
+        break;
+      case Btn.divide:
+        result = num1 / num2;
+        break;
+      default:
+    }
+
+    setState(() {
+      String expression =
+          "$number1 $operand $number2 = ${result.toStringAsPrecision(3)}";
+      history.add(expression);
+
+      number1 = result.toStringAsPrecision(3);
+      if (number1.endsWith(".0")) {
+        number1 = number1.substring(0, number1.length - 2);
+      }
+      operand = "";
+      number2 = "";
+
+      // Save history to Firestore
+      saveHistory();
+    });
+  }
+
+  void convertToPercentage() {
+    if (number1.isNotEmpty && operand.isNotEmpty && number2.isNotEmpty) {
+      calculate();
+    }
+
+    if (operand.isNotEmpty) {
+      return;
+    }
+
+    final number = double.parse(number1);
+    setState(() {
+      number1 = "${(number / 100)}";
+      operand = "";
+      number2 = "";
+    });
+  }
+
+  void clearAll() {
+    setState(() {
+      number1 = "";
+      operand = "";
+      number2 = "";
+    });
+  }
+
+  void delete() {
+    if (number2.isNotEmpty) {
+      number2 = number2.substring(0, number2.length - 1);
+    } else if (operand.isNotEmpty) {
+      operand = "";
+    } else if (number1.isNotEmpty) {
+      number1 = number1.substring(0, number1.length - 1);
+    }
+
+    setState(() {});
+  }
+
+  void appendValue(String value) {
+    if (value != Btn.dot && int.tryParse(value) == null) {
+      if (operand.isNotEmpty && number2.isNotEmpty) {
+        calculate();
+      }
+      operand = value;
+    } else if (number1.isEmpty || operand.isEmpty) {
+      if (value == Btn.dot && number1.contains(Btn.dot)) return;
+      if (value == Btn.dot && (number1.isEmpty || number1 == Btn.n0)) {
+        value = "0.";
+      }
+      number1 += value;
+    } else if (number2.isEmpty || operand.isNotEmpty) {
+      if (value == Btn.dot && number2.contains(Btn.dot)) return;
+      if (value == Btn.dot && (number2.isEmpty || number2 == Btn.n0)) {
+        value = "0.";
+      }
+      number2 += value;
+    }
+
+    setState(() {});
+  }
+
+  Color getBtnColor(String value) {
+    return [Btn.del, Btn.clr].contains(value)
+        ? Colors.blueGrey
+        : [
+            Btn.per,
+            Btn.multiply,
+            Btn.add,
+            Btn.subtract,
+            Btn.divide,
+            Btn.calculate,
+          ].contains(value)
+            ? Colors.orange
+            : Colors.black87;
+  }
+
+  void saveHistory() {
+    final historyRef = FirebaseFirestore.instance
+        .collection('History')
+        .doc('t3u6CXPV4V1CfhdrUXz0');
+    historyRef.set({
+      'calculation': history,
+    }, SetOptions(merge: true));
+  }
+
+  void loadHistory() async {
+    final historyRef = FirebaseFirestore.instance
+        .collection('History')
+        .doc('t3u6CXPV4V1CfhdrUXz0');
+    final doc = await historyRef.get();
+    if (doc.exists) {
+      setState(() {
+        history = List<String>.from(doc.data()?['calculation'] ?? []);
+      });
+    }
+  }
 }
