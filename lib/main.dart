5c44ffb2ff7404538a6bd6262946f46ce2654a65
Darshbir Singh <darshbir86@gmail.com>
2024-08-09 15:17:52 +0530

    Working Mechanism Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:first_app/firebase_options.dart';
 import 'package:flutter/material.dart';
-
 import 'Calculator_Screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
